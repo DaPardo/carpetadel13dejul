bf06951539103386ee09634e4634feb2619ccd16
DaPardo <DaPardo@unal.edu.co>
2021-07-13 20:39:42 -0400

    trecedejulio

---
+++ src/Clase29jun.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Clase29jun {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ingrese una palabra: ");
+        String entrada = scanner.nextLine();
+        System.out.println(entrada + " .Esa fué su palabra");
+
+
+
+        /*
+        var horario = "Lunes, martes y jueves";
+
+        System.out.println(horario);
+        System.out.println("Hello, World!");
+        System.out.println("Daniel Pardo");
+        */
+    }
+}
